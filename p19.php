de8fb730f83e7ce8503c7e84f8203038235b140a
upasna3444 <upasna3444@gmail.com>
2025-08-31 20:28:31 +0530

    Add files via upload

---
+++ p19.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "<center>";
+
+
+
+echo "<tr><td colspan='4'>Name : XYZ</td></tr>";
+echo "<table border='1' cellpadding='5' cellspacing='0'>";
+echo "<tr><th>Subject</th><th>Max Marks</th><th>Obtained Marks</th></tr>";
+
+echo "<tr><td>Sub1</td><td>50</td><td>40</td></tr>";
+echo "<tr><td>Sub2</td><td>50</td><td>30</td></tr>";
+echo "<tr><td>Sub3</td><td>50</td><td>39</td></tr>";
+echo "<tr><td>Sub4</td><td>50</td><td>45</td></tr>";
+echo "<tr><td>Sub5</td><td>50</td><td>43</td></tr>";
+
+echo "<tr><td><b>Total</b></td><td>250</td><td>197</td></tr>";
+echo "<tr><td colspan='2'><b>Percentage</b></td><td>78.8%</td></tr>";
+echo "<tr><td colspan='3'><b>Result : Passed with Distinction</b></td></tr>";
+echo "</table>";
+echo "</center>";
+?>
