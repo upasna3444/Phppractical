41a6bb327271b07fde3ac47d43e84a160df652a1
upasna3444 <upasna3444@gmail.com>
2025-08-31 20:25:23 +0530

    Add files via upload

---
+++ p16.php
@@ -0,0 +1,32 @@
+<?php
+//for loop
+echo "For Loop : =";
+echo "<br>";
+for($i=1;$i<=20;$i++){
+    echo "$i";
+    echo "<br>";
+}
+echo "<br>";
+// while
+echo "While Loop : =";
+echo "<br>";
+$i=1;
+while($i<=20){
+    echo "$i";
+    echo "<br>";
+    $i++;
+
+}
+echo "<br>";
+//do while
+echo "Do While Loop : =";
+echo "<br>";
+$i=1;
+do{
+ echo "$i";
+ echo "<br>";
+ $i++;
+
+}while($i<=20);
+
+?>
